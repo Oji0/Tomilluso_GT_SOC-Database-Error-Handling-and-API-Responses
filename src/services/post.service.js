aa24f59a144cb4b83d20df3b76c9339f1c76a533
OjiBokuto <prince2004.tomilluso@gmail.com>
2025-09-13 15:36:25 +0800

    Update: post.service.js

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,51 +1,60 @@
-let posts = [
-    { id: 1, title: 'First Post', content: 'This is the first post.' },
-    { id: 2, title: 'Second Post', content: 'This is the second post.' }
-];
-let nextId = 3;
+// let posts = [
+//     { id: 1, title: 'First Post', content: 'This is the first post.' },
+//     { id: 2, title: 'Second Post', content: 'This is the second post.' }
+// ];
+// let nextId = 3;
 
-export const getAllPosts = () => {
+// export const getAllPosts = () => {
+//     return posts;
+// };
+
+// export const getPostById = (id) => {
+//     return posts.find(p => p.id === id);
+// };
+
+// export const createPost = (postData) => {
+//     const newPost = { id: nextId++, ...postData };
+//     posts.push(newPost);
+//     return newPost;
+// };
+
+// export const updatePost = (id, postData) => {
+//     const postIndex = posts.findIndex(p => p.id === id);
+//     if (postIndex === -1) {
+//         return null;
+//     }
+//     posts[postIndex] = { ...posts[postIndex], ...postData };
+//     return posts[postIndex];
+// };
+
+// export const updatePostPartial = (id, updates) => {
+//     const postIndex = posts.findIndex(p => p.id === id);
+//     if (postIndex === -1) {
+//         return null;
+//     }
+
+//     posts[postIndex] = {
+//         ...posts[postIndex],
+//         ...updates
+//     };
+
+//     return posts[postIndex];
+// };
+
+// export const deletePost = (id) => {
+//     const postIndex = posts.findIndex(p => p.id === id);
+//     if (postIndex === -1) {
+//         return false;
+//     }
+//     posts.splice(postIndex, 1);
+//     return true;
+// };
+
+
+// src/services/post.service.js
+import pool from '../config/db.js';
+
+export const getAllPosts = async () => {
+    const [posts] = await pool.query('SELECT * FROM posts');
     return posts;
-};
-
-export const getPostById = (id) => {
-    return posts.find(p => p.id === id);
-};
-
-export const createPost = (postData) => {
-    const newPost = { id: nextId++, ...postData };
-    posts.push(newPost);
-    return newPost;
-};
-
-export const updatePost = (id, postData) => {
-    const postIndex = posts.findIndex(p => p.id === id);
-    if (postIndex === -1) {
-        return null;
-    }
-    posts[postIndex] = { ...posts[postIndex], ...postData };
-    return posts[postIndex];
-};
-
-export const updatePostPartial = (id, updates) => {
-    const postIndex = posts.findIndex(p => p.id === id);
-    if (postIndex === -1) {
-        return null;
-    }
-
-    posts[postIndex] = {
-        ...posts[postIndex],
-        ...updates
-    };
-
-    return posts[postIndex];
-};
-
-export const deletePost = (id) => {
-    const postIndex = posts.findIndex(p => p.id === id);
-    if (postIndex === -1) {
-        return false;
-    }
-    posts.splice(postIndex, 1);
-    return true;
 };
