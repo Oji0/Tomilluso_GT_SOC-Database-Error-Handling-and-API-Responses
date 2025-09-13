ed56681cb41e4ec12b1b877d3dfb54ca5054516a
OjiBokuto <prince2004.tomilluso@gmail.com>
2025-09-13 20:30:06 +0800

    Update: db.js

--- src/config/db.js
+++ src/config/db.js
@@ -5,10 +5,10 @@ dotenv.config(); // Loads environment variables from .env file
 
 // Create a connection pool
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: localhost,
+    user: root,
+    password: admin,
+    database: blogdatabase,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
