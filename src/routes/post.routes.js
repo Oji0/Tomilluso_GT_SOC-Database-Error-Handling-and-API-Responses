74e3588c2dd7d101448609c89c5bca47740b2046
OjiBokuto <prince2004.tomilluso@gmail.com>
2025-09-13 21:30:45 +0800

    Update: post.routes.js

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,7 +1,12 @@
 import { Router } from 'express';
 import * as postController from '../controllers/post.controller.js';
+import { validatePost } from '../middlewares/validator.middleware.js';
 
 const router = Router();
+
+router.post('/', validatePost, postController.createPost);
+router.put('/:id', validatePost, postController.updatePost);
+router.patch('/:id', postController.partiallyUpdatePost);
 
 router.get('/', postController.getAllPosts);
 router.post('/', postController.createPost);
