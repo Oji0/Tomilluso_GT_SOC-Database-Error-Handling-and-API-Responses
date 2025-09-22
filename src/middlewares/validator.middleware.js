0f780c0a1830f65804ecdd3129bb327ed3aeb3a5
OjiBokuto <prince2004.tomilluso@gmail.com>
2025-09-22 13:28:39 +0800

    Update validator.middleware.js

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -1,19 +1,20 @@
 import { body, validationResult } from 'express-validator';
 
 export const validatePost = [
-    // Title must not be empty and is sanitized
     body('title')
         .trim()
         .notEmpty()
         .withMessage('Title is required.'),
 
-    // Content must not be empty and is sanitized
     body('content')
         .trim()
         .notEmpty()
         .withMessage('Content is required.'),
 
-    // This function handles the result of the validations
+    body('authorId')
+        .isInt({ min: 1 })
+        .withMessage('A valid author ID is required.'),
+
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
