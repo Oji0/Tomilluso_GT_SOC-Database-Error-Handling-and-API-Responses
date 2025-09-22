6550d5dee4dcd850bcf907d623bf65b6ef8fa79c
OjiBokuto <prince2004.tomilluso@gmail.com>
2025-09-22 13:23:19 +0800

    Add: user.controller.js

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,18 @@
+import * as userService from '../services/user.service.js';
+import { ApiResponse } from '../utils/ApiResponse.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
+
+export const createUser = asyncHandler(async (req, res) => {
+  const newUser = await userService.createUser(req.body);
+  res.status(201).json(new ApiResponse(201, newUser, 'User created successfully'));
+});
+
+export const getUserById = asyncHandler(async (req, res) => {
+  const user = await userService.getUserById(req.params.id);
+  res.status(200).json(new ApiResponse(200, user, 'User fetched successfully'));
+});
+
+export const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await userService.getAllUsers();
+  res.status(200).json(new ApiResponse(200, users, 'All users fetched successfully'));
+});
